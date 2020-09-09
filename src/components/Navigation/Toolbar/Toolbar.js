04b3b9e314a0b5601b200a76dc9c2b4ae76eaf0c
Bruno <bruno.navarreteon@gmail.com>
2020-09-09 08:51:16 -0500

    SideDrawer styling

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,10 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 const toolbar = (props) => (
 	<header className={ classes.Toolbar }>
 		<div>MENU</div>
-		<Logo />
-		<NavigationItems />
+		<Logo height="80%" />
+		<nav className={ classes.DesktopOnly }>
+			<NavigationItems />
+		</nav>
 	</header>
 )
 
